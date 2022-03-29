c519d7ce9e146a0e1ad5d3a6be2bea4f6f1656c6
Cade Jorgensen <cade25k@byu.edu>
2022-03-28 19:19:33 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BowlersDbContext context { get; set; }
+        public HomeController(BowlersDbContext bc)
+        {
+            context = bc;
+        }
+
+        public IActionResult Index()
+        {
+            var bowlers = context.Bowlers.ToList();
+            return View(bowlers);
+        }
+    }
+}
---
+++ Models/Bowler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Models
+{
+    public class Bowler
+    {
+        [Key]
+        [Required]
+        public int BowlerID { get; set; }
+        [Required]
+        public string BowlerFirstName { get; set; }
+        public char BowlerMiddleInit { get; set; }
+        [Required]
+        public string BowlerLastName { get; set; }
+        [Required]
+        public string BowlerAddress { get; set; }
+        [Required]
+        public string BowlerCity { get; set; }
+        [Required]
+        public string BowlerState { get; set; }
+        [Required]
+        public string BowlerZip { get; set; }
+        [Required]
+        public string BowlerPhoneNumber { get; set; }
+        [Required]
+        public int TeamID { get; set; }
+    }
+}
